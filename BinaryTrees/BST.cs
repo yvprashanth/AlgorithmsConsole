a3990e5e1454f1f92b580aa3e46914d560b1e589
prashanth <tomu644@gmail.com>
2015-01-04 16:55:50 -0700

    commit as new project

--- BinaryTrees/BST.cs
+++ BinaryTrees/BST.cs
@@ -31,6 +31,13 @@ namespace AlgorithmsConsole.BinaryTrees
             }
         }
 
+        public static void InOrderIterative(BST tree)
+        {
+            if (tree == null)
+                return;
+
+        }
+
         public static void PreOrderIterative(BST tree)
         {
             Stack<BST> stack = new Stack<BST>();
